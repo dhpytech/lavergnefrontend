152e88db32f316484b35b038c142bbe6637ccaab
dh tech <dh.tech.gentle@gmail.com>
2025-12-30 20:10:21 +0700

    FE02

--- src/hooks/useMarisMetadata.tsx
+++ src/hooks/useMarisMetadata.tsx
@@ -6,17 +6,19 @@ export const useMarisMetadata = () => {
   const [data, setData] = useState({
     employees: [] as any[],
     productCodes: [] as string[],
-    stopCodes: ["S01 - Mechanical", "S02 - Electrical", "S03 - Setup"],
-    problemCodes: ["Q01 - Color", "Q02 - Black Spot", "Q03 - Moisture"],
+    stopCodes: [],
+    problemCodes: [],
   });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [empRes, prodRes] = await Promise.all([
+        const [empRes, prodRes,stopRes,problemRes] = await Promise.all([
           axios.get('http://127.0.0.1:8000/employee/employee/'),
-          axios.get('http://127.0.0.1:8000/itemcode/items-code/')
+          axios.get('http://127.0.0.1:8000/itemcode/items-code/'),
+          axios.get('http://127.0.0.1:8000/stoptime/stop-time/'),
+          axios.get('http://127.0.0.1:8000/problem/problems/'),
         ]);
 
         setData(prev => ({
@@ -24,7 +26,9 @@ export const useMarisMetadata = () => {
           // Sử dụng employee_name từ API của bạn
           employees: empRes.data,
           // Sử dụng item_name từ API của bạn
-          productCodes: prodRes.data.map((item: any) => item.item_name)
+          productCodes: prodRes.data.map((item: any) => item.item_name),
+          stopCodes: stopRes.data.map((item: any)=> item.stop_time_name),
+          problemCodes: problemRes.data.map((item:any)=> item.problem_code),
         }));
       } catch (error) {
         console.error("Lỗi API, kiểm tra CORS hoặc Server:", error);
